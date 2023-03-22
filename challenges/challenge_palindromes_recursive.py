8e17fb7cb1fd8001f995ff500b843edf97aa387e
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-03-22 17:37:02 -0300

    req03

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    if len(word) == 1:
+        return True
+    if len(word) == 2:
+        return word[0] == word[1]
+    if word[0] != word[-1]:
+        return False
+    return is_palindrome_recursive(word[1:-1], low_index, high_index)

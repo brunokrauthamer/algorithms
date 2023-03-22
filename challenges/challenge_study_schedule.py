d2912e312aa9370d4c2bd44fc4a4f27d8671ea42
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-03-22 17:05:06 -0300

    req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(target_time) != int:
+        return None
+    counter = 0
+    for period in permanence_period:
+        if type(period[0]) != int or type(period[1]) != int:
+            return None
+        if period[0] <= target_time <= period[1]:
+            counter += 1
+    return counter
